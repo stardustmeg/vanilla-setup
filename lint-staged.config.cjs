0f762a606f2105bbe85ef02da798f347ad638c40
Margarita Golubeva <margaret.golubeva@gmail.com>
2026-03-03 17:00:31 +0300

    build: set up project structure with initial configuration files

---
+++ lint-staged.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{js,html,css}': 'biome check --write',
+  '*.css': 'stylelint --fix',
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector('h1')
+
+if (heading) {
+  heading.dataset.ready = 'true'
+}
+
+// biome-ignore lint/suspicious/noConsole: <just because>
+console.log('Hello, kitten! ❤️‍🔥')
